31e322ae59438d4634fcfdea7ad45f7cc02a36a0
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2021-05-10 09:42:17 -0400

    added project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject net.clojars.yogthos/hiccup-template "0.1.2"
+  :description "minimal templating engine using EDN"
+  :url "https://github.com/yogthos/hiccup-template"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"})
